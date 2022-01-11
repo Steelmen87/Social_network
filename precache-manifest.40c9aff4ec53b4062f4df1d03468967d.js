ec7d4bd1090e99576db548b35089ac9404d0aa6f
Konstantin <Steelmen87@bk.ru>
2022-01-11 18:26:26 +0300

    Updates

---
+++ precache-manifest.40c9aff4ec53b4062f4df1d03468967d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9137e0ac08f7e86601ec05a105a9b53e",
+    "url": "/Social_network/index.html"
+  },
+  {
+    "revision": "86b60e8525d24585d852",
+    "url": "/Social_network/static/css/2.486ebae0.chunk.css"
+  },
+  {
+    "revision": "c0145eadb9099dbc791d",
+    "url": "/Social_network/static/css/main.96c50afe.chunk.css"
+  },
+  {
+    "revision": "86b60e8525d24585d852",
+    "url": "/Social_network/static/js/2.d5052a23.chunk.js"
+  },
+  {
+    "revision": "c0145eadb9099dbc791d",
+    "url": "/Social_network/static/js/main.7b3f7b8d.chunk.js"
+  },
+  {
+    "revision": "dc8aa9fa9195e69421f9",
+    "url": "/Social_network/static/js/runtime~main.93ffd6f6.js"
+  },
+  {
+    "revision": "6921c9f6ff89071a37a99cecd0bc0ed3",
+    "url": "/Social_network/static/media/Hourglass.6921c9f6.gif"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/Social_network/static/media/user.8b1dbb1f.png"
+  }
+]);
