e210eb1033e51f547cd0a3e39f1eb4fdd9d8f8bc
Konstantin <Steelmen87@bk.ru>
2022-01-11 19:08:23 +0300

    HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/redux-store'
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import App from "./App";
 
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root'));
 
 serviceWorker.unregister();
